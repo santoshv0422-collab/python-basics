660432e2615031f08e94a52951f3472e16303d80
santoshv0422-collab <santoshv0422@gmail.com>
2026-06-25 16:55:55 +0530

    code

---
+++ test.py
@@ -0,0 +1,18 @@
+print("hello world","world",sep="#")
+# print("hello world",end="")
+# print("hello world")
+x="10.5"
+# y="s"
+# print("abc"="abd")
+# x=int(input())
+print(int(float(x)))
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+print("""hello
+      my name is 
+      john
+      """)
+x="hello"
+print(x[0:3:2]) # start:end:step
+print(x[::-1])
